48deb2a84cf988bbf0f0c4f7c7f8d970b92a71e9
miba1018 <bazantm@gmail.com>
2024-02-21 15:41:04 +0100

    new line added

--- src/Task1.java
+++ src/Task1.java
@@ -7,5 +7,7 @@ public class Task1 {
         System.out.println("My name is: ");
         System.out.println("My university is: ");
         System.out.println("My field of study is: ");
+
+        System.out.println("new line");
     }
 }
