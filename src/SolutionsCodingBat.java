5ee176019829ff36bd2c4d51f9f17729b47c3177
miba1018 <bazantm@gmail.com>
2024-02-21 15:37:57 +0100

    added solution for 1st example from codingbat.com

---
+++ src/SolutionsCodingBat.java
@@ -0,0 +1,7 @@
+public class SolutionsCodingBat {
+    public static void main(String[] args) {
+        System.out.println("Solution ot 1st example from codingbat.com");
+
+        System.out.println("");
+    }
+}
---
+++ src/Task1.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Task1 {
+
+
+    public static void main(String[] args) {
+        System.out.println("My name is: ");
+        System.out.println("My university is: ");
+        System.out.println("My field of study is: ");
+    }
+}
