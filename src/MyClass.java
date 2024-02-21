bc224fdfbb6254b93e938aeb925e37722180b0fb
miba1018 <bazantm@gmail.com>
2024-02-21 15:10:12 +0100

    init commit

---
+++ src/MyClass.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MyClass {
+
+
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        double a = 5.5;
+        System.out.println("a = " + a);
+
+        int b = (int) a;
+        System.out.println("a = " + a);
+
+        System.out.println("This is a new line");
+
+
+    }
+}
